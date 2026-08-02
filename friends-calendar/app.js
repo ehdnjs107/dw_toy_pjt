7eae9b73f4f487793ff08e29d21286818dc93440
Dowon Kim <ehwldnjs111@gmail.com>
2026-08-03 03:43:10 +0900

    Reset calendar onboarding walkthrough

--- friends-calendar/app.js
+++ friends-calendar/app.js
@@ -7,8 +7,8 @@ import { getDatabase, get, onValue, ref, serverTimestamp, set, update } from "ht
 
   const STORAGE_PREFIX = "dw-friends-calendar:";
   const CHANNEL_NAME = "dw-friends-calendar-sync";
-  const APP_VERSION = "20260803-5";
-  const ONBOARDING_VERSION = "1";
+  const APP_VERSION = "20260803-6";
+  const ONBOARDING_VERSION = "2";
   const DEFAULT_TITLE = "친구 약속 잡기";
   const DEFAULT_THRESHOLD = 0.8;
   const DEFAULT_PARTICIPANTS = 6;
